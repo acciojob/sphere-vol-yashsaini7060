2ef923e8f3c3512ab855bb3d719a759732a82b15
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-29 01:11:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function volume_sphere() {
     //Write your code here
-  
+	  const radius = parseInt(document.getElementById("radius").innerText);
+	  const volumeDiv=document.getElementById("volume");
+	  const volume=(4/3)*Math.PI*radius*radius*radius; 
+	  volumeDiv.innerHTML=volume;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
